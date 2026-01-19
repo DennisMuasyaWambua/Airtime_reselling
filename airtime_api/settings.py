cb97e4cb2e8482c380e1c0c8a2132ff838fea025
Dennis wambua <muasyathegreat4@gmail.com>
2026-01-19 09:39:00 +0300

    localhost added to allowed CORS origin

--- airtime_api/settings.py
+++ airtime_api/settings.py
@@ -44,16 +44,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'airtime',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://airtimereselling-production.up.railway.app",
 ]
 
 ROOT_URLCONF = 'airtime_api.urls'
