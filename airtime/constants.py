9e9968496a6c60968994cf22989fa3512e6e4ff4
Dennis wambua <muasyathegreat4@gmail.com>
2026-04-15 13:34:42 +0300

    airtime top up

--- airtime/constants.py
+++ airtime/constants.py
@@ -1,4 +1,4 @@
 DARAJA_ENDPOINTS = {
-    'access_token':'https://api.safaricom.co.ke/oauth/v1/generate',
-    'airtime_topup':'https://api.safaricom.co.ke/v1/pretups/api/recharge'
+    'access_token': 'https://api.safaricom.co.ke/oauth2/v1/generate?grant_type=client_credentials',
+    'airtime_topup': 'https://api.safaricom.co.ke/v1/pretups/api/recharge'
 }
